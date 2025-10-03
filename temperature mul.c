d8d10c6b2f1773a7e98d0d8a5d0dab38bd7c8f80
Anushka020806 <anushkamore0000@gmail.com>
2025-10-03 09:20:20 +0530

    Update and rename temperature.c to temperature mul.c

---
+++ temperature mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    float A,B,mul;
+       printf("Enter the temp of sensor A\n");
+       scanf("%f",&A);
+
+        printf("Enter the temp of sensor B\n");
+        scanf("%f",&B);
+
+        mul=A*B;
+
+        printf("total temperature of the sensor is: %2f", mul);
+
+
+    }    
